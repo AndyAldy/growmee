94b329271d0bee3195909fbe76cbd8fa3998ab1a
AndyAldy <andyaldyansyah11@gmail.com>
2025-06-08 12:05:47 +0700

    editted

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -4,9 +4,11 @@ allprojects {
         mavenCentral()
     }
 }
-    plugins {
-        id("com.google.gms.google-services") version "4.4.2" apply false
-    }
+plugins {
+    id("com.android.application") apply false
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
+}
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
